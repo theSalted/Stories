7afa283e0b612dce8576294a063c43096e3edf90
Yuhao Chen <yuhao.chen2000@icloud.com>
2021-11-12 15:32:17 +0800

    Initial Commit

---
+++ Stories/StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesApp.swift
+//  Stories
+//
+//  Created by 陈宇昊 on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
